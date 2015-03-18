5dfd9cd921907fcabf4fc6abace2a9f04ba2abb9
Isaac Lyman <ilyman@mail.mtc.byu.edu>
2015-03-18 15:07:26 -0600

    Further structures

--- client/home.js
+++ client/home.js
@@ -1,4 +1,11 @@
+if(Meteor.isClient) {
+
 // Enable tooltips
+Template.home.rendered = function() {
+	$('[data-toggle="tooltip"]').tooltip({'placement': 'top'});
+};
+
+// Set page title dynamically
 Handlebars.registerHelper("setTitle", function(title) {
 	if(title) {
 		document.title = title;
@@ -7,20 +14,41 @@ Handlebars.registerHelper("setTitle", function(title) {
 	}
 })
 
-Template.home.rendered = function() {
-	$('[data-toggle="tooltip"]').tooltip();
-}
-
 Template.home.events({
-	"change #nameInput": function (event) {
-		console.log(event);
+	"keypress #nameInput": function (event) {
 		return;
 	},
-	"submit .begin": function (event) {
-		var name  = event.target[0].value;
-		var zip   = event.target[1].value;
-		var hobby = event.target[2].value;
-		console.log(name, zip, hobby);
+	"keypress #zipInput": function (event) {
+		return;
+	},
+	"keypress #hobbyInput": function (event) {
+
+	},
+	"submit #begin": function (event) {
+		var name  = event.target[0].value.toString();
+		var zip   = event.target[1].value.toString().substring(0,3);
+		var hobby = event.target[2].value
+					.toString()
+					.trim()
+					.toLowerCase()
+					.replace(/[^\w]|_/g, '');
+		var board = zip + hobby;
+		if( !Boards.find({ board: board }) ) {
+			var newBoard = {
+				board: board,
+				hobby: hobby,
+				zip  : zip,
+				description: '',  //TODO
+				createdDate: null //TODO
+			}
+			Meteor.makeNewBoard(newBoard);
+		}
+		Session.set('user', name);
+		Session.set('zip', zip);
+		Session.set('board', hobby);
+		Router.go('/zip/' + zip + '/board/' + hobby);
 		return false;
 	}
 });
+
+}
