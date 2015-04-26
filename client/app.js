14a0fe71e999bfa9d93120fce2b8979d0ed8496a
Isaac Lyman <mail@isaaclyman.com>
2015-04-26 10:35:23 -0600

    Add, then remove css transitions between routes

--- client/app.js
+++ client/app.js
@@ -7,6 +7,7 @@ Handlebars.registerHelper("setTitle", function(title) {
 	}
 });
 
+// Add favicon programmatically (a workaround)
 Template.default.rendered = function() {
 	var link = document.createElement('link');
     link.type = 'image/png';
@@ -16,6 +17,7 @@ Template.default.rendered = function() {
     document.getElementsByTagName('head')[0].appendChild(link);
 };
 
+// Verify email via link
 Accounts.onEmailVerificationLink(function(token, done) {
 	Accounts.verifyEmail(token);
 	Session.setPersistent('andVerified', true);
