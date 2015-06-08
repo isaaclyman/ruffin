6463168ab08d2c7404b1533f926cc7fe4d3ab82f
Isaac Lyman <mail@isaaclyman.com>
2015-06-07 21:14:25 -0600

    Change to more narrow subscribe statements (broken)

--- server/subscriptions.js
+++ server/subscriptions.js
@@ -2,9 +2,18 @@
 	PUBLISH
 */
 
-// All boards are public
-Meteor.publish('boards', function() {
-	return Boards.find({}, {fields: {described_by: 0}});
+// Publish a specific board
+Meteor.publish('board', function(region, hobby) {
+	region = app.transform.region(region);
+	hobby = app.transform.humanToUrl(hobby);
+	return Boards.find({ zip: region, hobby: hobby }, 
+					   { fields: { described_by: 0 } });
+});
+
+// Publish a list of boards from a specific region
+Meteor.publish('localBoards', function(region) {
+	region = app.transform.region(region);
+	return Boards.find({ zip: region }, { fields: { hobby: 1 } });
 });
 
 // Prevent users from editing their profile
